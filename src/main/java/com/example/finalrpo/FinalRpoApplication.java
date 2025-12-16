e5f7d444b2128214b242e06a518d2ca69d74444c
Makegoiii <magzhankydyrbai05@gmail.com>
2025-12-16 23:01:36 +0500

    initial commit

---
+++ src/main/java/com/example/finalrpo/FinalRpoApplication.java
@@ -0,0 +1,13 @@
+package com.example.finalrpo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalRpoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinalRpoApplication.class, args);
+    }
+
+}
